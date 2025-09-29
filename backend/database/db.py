f6d7f1d6f6ee4fe7f088f8ce6f51a77e4314cde7
ninyx <theebatican@gmail.com>
2025-09-29 21:19:57 +0800

    .env setup for database connection

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://expense_user:expense_pass@localhost:5432/expense_tracker"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
